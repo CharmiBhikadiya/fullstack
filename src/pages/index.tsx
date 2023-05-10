ab3a83670a528df0587948c7df3ebbe5b2d3d84e
Charmi Bhikadiya <>
2023-05-09 19:38:58 -0700

    add name, summary,  projects button click function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,32 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
+import {useRouter} from 'next/router'
+import { Button } from '@mui/material';
+import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({name, summary}) {
+  const router = useRouter()
   return (
-    <>
-      <main>
-        <div className={styles.description}>
-          <div className={styles.grid}>
-          <div>
-              <a className={styles.card} href='/index.tsx'>Home</a>
-            </div>
-            <div>
-              <a className={styles.card} href='/about'>About</a>
-            </div>
-            <div>
-              <a className={styles.card} href='/projects'>Projects</a>
-            </div>
-            <div>
-              <a className={styles.card} href='/contact'>Contact</a>
-            </div>
-          </div>
+    <section className={styles.Home}>
+        <h1 className={styles.Name}>{name}</h1>
+        <div className={styles.Summary}>{summary}</div>
+        <div>
+          <Button 
+            variant='contained' 
+            size='large' 
+            onClick={() => router.push('/projects')}
+          >
+            Projects
+          </Button>
         </div>
-      </main>
-    </>
-  )
+    </section>
+  );
+}
+
+export async function getStaticProps() {
+  //get data from an api
+  return {
+    props: {
+    name: "Charmi Bhikadia",
+    summary: 'Experienced full-stack developer with a track record of delivering high-quality software solutions. Expertise in designing and implementing user-friendly interfaces, optimizing database performance, and ensuring code maintainability and scalability.'
+    }, 
+ }
 }
