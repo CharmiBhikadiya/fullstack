33cac0f616a5b72feaf089ecda0ecfcf38b5ea54
Charmi Bhikadiya <>
2023-05-09 18:21:29 -0700

    route setup

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,3 +1,3 @@
 export default function About() {
-    return <h1>New About Page</h1>
+    return <h1>About</h1>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,11 +16,22 @@ export default function Home() {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
-          <p>
+          <div className={styles.grid}>
+            <div>
+              <a href='/about'>About</a>
+            </div>
+            <div>
+              <a href='/projects'>Projects</a>
+            </div>
+            <div>
+              <a href='/contact'>Contact</a>
+            </div>
+          </div>
+          {/* <p>
             Hello Get started by editing&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
+          </p> */}
+          {/* <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
@@ -36,10 +47,10 @@ export default function Home() {
                 priority
               />
             </a>
-          </div>
+          </div> */}
         </div>
 
-        <div className={styles.center}>
+        {/* <div className={styles.center}>
           <Image
             className={styles.logo}
             src="/next.svg"
@@ -48,9 +59,9 @@ export default function Home() {
             height={37}
             priority
           />
-        </div>
+        </div> */}
 
-        <div className={styles.grid}>
+        {/* <div className={styles.grid}>
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
@@ -107,7 +118,7 @@ export default function Home() {
               with&nbsp;Vercel.
             </p>
           </a>
-        </div>
+        </div> */}
       </main>
     </>
   )
