1974122be402bccfd3ce51297884d503f1ccbfc6
Charmi Bhikadiya <>
2023-05-10 18:32:43 -0700

    about page SSG and ISR

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,8 +1,8 @@
-import { Button, Grid } from "@mui/material";
+import { Button, Grid, Chip, Stack } from "@mui/material";
 import {useRouter} from 'next/router'
 import PageDescription from "../../components/PageDescription";
 
-export default function AboutPage() {
+export default function AboutPage({skills}) {
     const router = useRouter()
     return ( <section>
         <PageDescription 
@@ -11,9 +11,9 @@ export default function AboutPage() {
     <Grid container spacing={2}>
         <Grid item md={6}>
             <h2>Get to know me!</h2>
-            <p>I'm a Frontend Web Developer building the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section.</p>
+            <p>I&aposm a Frontend Web Developer building the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section.</p>
             <p>I also like sharing content related to the stuff that I have learned over the years in Web Development so it can help other people of the Dev Community. Feel free to Connect or Follow me on my Linkedin where I post useful content related to Web Development and Programming</p>
-            <p>I'm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don't hesitate to contact me.</p>
+            <p>I&aposm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don&apost hesitate to contact me.</p>
             <Button 
             variant='contained' 
             size='large' 
@@ -24,10 +24,33 @@ export default function AboutPage() {
         </Grid>
         <Grid item md={6}>
             <h2>My Skills</h2>
+            <Stack direction="row" spacing={2} useFlexGap flexWrap="wrap">
+            { skills.map( (skill) => (
+                <Chip key={skill} label={skill} />
+            )) }
+            </Stack>
         </Grid>
     </Grid>
     </section>
     )
+}
+export async function getStaticProps (){
+    let skills = [];
+  
+    try {
+        const response = await fetch('https://charmi-skills-api-default-rtdb.firebaseio.com/skills.json')
+        const data = await response.json();
+        skills = data.split(',')        
+    } catch (error) {
+        console.error(error)
+    }
+
+    return {
+        props: {
+            skills
+        },
+        revalidate:30
+    }
 }
 
 //useRouter is a hook, helps to navigate to /contact on button onClick
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,25 @@
-import {useRouter} from 'next/router'
-import { Button } from '@mui/material';
-import styles from '../styles/Home.module.css'
+import { useRouter } from "next/router";
+
+import { Button } from "@mui/material";
+
+import styles from "../styles/Home.module.css";
 
 export default function Home({ name, summary }) {
-  const router = useRouter()
+  const router = useRouter();
+
   return (
     <section className={styles.Home}>
-        <h1 className={styles.Name}>{name}</h1>
-        <div className={styles.Summary}>{summary}</div>
-        <div>
-          <Button 
-            variant='contained' 
-            size='large' 
-            onClick={() => router.push('/projects')}
-          >
-            Projects
-          </Button>
-        </div>
+      <h1 className={styles.Name}>{name}</h1>
+      <div className={styles.Summary}>{summary}</div>
+      <div>
+        <Button
+          variant="contained"
+          size="large"
+          onClick={() => router.push("/projects")}
+        >
+          Projects
+        </Button>
+      </div>
     </section>
   );
 }
