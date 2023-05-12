4ed585561113bed4fff10f621b14e5649aca9fde
Charmi Bhikadiya <>
2023-05-11 19:42:08 -0700

    add clear and submit button

--- src/components/modals/AddNewProjectModal.js
+++ src/components/modals/AddNewProjectModal.js
@@ -1,21 +1,37 @@
-import { Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
 import ProjectForm from "../forms/ProjectForm";
 
-export default function AddNewProjectModal ({open, onClose}) {
-    return (
-        <Dialog 
-            open={open}
-            onClose={onClose}
-        >
-            <DialogTitle>
-                Addding a new project... 
-            </DialogTitle>
-            <DialogContent>
-                <ProjectForm />
-            </DialogContent>
-            <DialogActions>
+export default function AddNewProjectModal ({ open, onClose, onSubmit }) {
 
-            </DialogActions>
-        </Dialog>
-    )
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+    >
+      <DialogTitle>
+        Adding a new project...
+      </DialogTitle>
+      <DialogContent>
+        <ProjectForm onSubmit={onSubmit}/>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          variant="contained"
+          color="error"
+          type="reset"
+          form='project-form'
+        >
+          Clear form
+        </Button>
+        <Button
+          variant="contained"
+          type="submit"
+          form='project-form'
+          onClick={onClose}
+        >
+          Add project
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
 }
